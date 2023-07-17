b9b0f031f103ae0ddb8b19ce118d0b304a5d6a1d
Newton <newton.bundi@student.moringaschool.com>
2023-07-17 22:50:27 +0300

    Assignement completed but the npm test is indicating errors, whereas I have fulfilled all that was required

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    console.log(`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity="go to the office"){
+    console.log(`This Monday, I will ${activity}`)
+}
+saturdayFun("bathe my dog");
+
+mondayWork("work from home");
+
+
+function wrapAdjective(arg="*"){
+    return function(para="special"){
+        console.log(`You are ${arg}${para}${arg}!`)
+    }
+}
+
+wrapAdjective("||")("a dedicated programmer");
